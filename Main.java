f3347ad6bceb6825e9e0c7493404e0ecc3d83d9e
benscodelibrary <ben.jacobs.personal@gmail.com>
2025-04-04 09:55:33 -0600

    Hello World Commit

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Set;
+import java.util.Stack;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
